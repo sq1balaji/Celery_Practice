d6bb4626ac8f850bfeaa34e37e7d1b268658c152
sq1balaji <balaji.p@sq1@security>
2025-05-23 00:36:47 -0700

    add a txt file.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from app.tasks.github_checker import check_for_new_commit
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "GitHub Commit Checker running with Celery"}
+@app.get("/trigger-check")
+def trigger_check():
+    check_for_new_commit.delay()
+    return {"status": "check task triggered"}
